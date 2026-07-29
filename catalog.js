e254c5cf29462431a03c6203e8a7cec5e1202d7a
marsmiaMIA <bga.andrews@gmail.com>
2026-07-29 17:58:59 +0100

    Add files via upload

---
+++ catalog.js
@@ -0,0 +1,18 @@
+const ITEMS = [
+        {
+          name: "Chop Plush & Bullet Whistle Set",
+          game: "GTA V",
+          category: "Hat",
+          description:
+            "Announced and released into the Rockstar Warehouse on 5th December 2013. A soft plush version of Chop which includes a removable leather collar with tag and a secret stash pocket which includes a silver effect bullet whistle which the GTA V logo etched on its side. Rarer to find in its original open-fronted box. The plush is approximately 15cm high and 25cm long. The leash and collar were also sold separately as a set.",
+          year: "2013",
+          rarity: "Rare",
+          availability: "Sold in Stores",
+          image: "images/chop.jpg",
+          extraImages: [],
+          link: "",
+        },
+
+        // ▼▼▼  ADD NEW ITEMS ABOVE THIS LINE  ▼▼▼
+        // (Copy the template block from the guide above, paste it here, fill in the details.)
+      ];
